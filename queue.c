a890963776dace80b9aa33240f74f8bbc8db4aac
Lisa <lisa.20845@gmail.com>
2024-03-04 12:34:01 +0800

    Implement sort function
    Use merge sort to implement the sort function
    Also add q_delete_mid, q_swap, and reverse functions

--- queue.c
+++ queue.c
@@ -127,11 +127,28 @@ bool q_delete_dup(struct list_head *head)
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
+    if (!head || list_empty(head))
+        return;
+    struct list_head *node;
+    list_for_each (node, head) {
+        if (node->next == head) {
+            break;
+        }
+        list_move(node, node->next);
+    }
     // https://leetcode.com/problems/swap-nodes-in-pairs/
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return;
+    }
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head)
+        list_move(node, head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
@@ -139,8 +156,48 @@ void q_reverseK(struct list_head *head, int k)
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
 }
 
+void q_merge_two(struct list_head *list1, struct list_head *list2)
+{
+    if (!list1 || !list2)
+        return;
+    struct list_head tmp_head;
+    INIT_LIST_HEAD(&tmp_head);
+    while (!list_empty(list1) && !list_empty(list2)) {
+        element_t *list1_element = list_first_entry(list1, element_t, list);
+        element_t *list2_element = list_first_entry(list2, element_t, list);
+        char *list1_str = list1_element->value,
+             *list2_str = list2_element->value;
+        element_t *mini =
+            (strcmp(list1_str, list2_str) < 0) ? list1_element : list2_element;
+        list_move_tail(&mini->list, &tmp_head);
+    }
+    list_splice_tail_init(list1, &tmp_head);
+    list_splice_tail_init(list2, &tmp_head);
+    list_splice(&tmp_head, list1);
+}
+
+void merge_sort(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *slow = head, *fast = head->next;
+    for (; fast != head && fast->next != head; fast = fast->next->next)
+        slow = slow->next;
+    struct list_head left;
+    list_cut_position(&left, head, slow);
+    merge_sort(&left);
+    merge_sort(head);
+    q_merge_two(head, &left);
+}
+
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend)
+{
+    merge_sort(head);
+    if (descend) {
+        q_reverse(head);
+    }
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
@@ -165,11 +222,3 @@ int q_merge(struct list_head *head, bool descend)
     // https://leetcode.com/problems/merge-k-sorted-lists/
     return 0;
 }
-
-// int main(){
-//     struct list_head *tmp = q_new();
-//     char *name = "Lisa";
-//     q_insert_head(tmp, name);
-//     printf("hello world!");
-//     return 0;
-// }
